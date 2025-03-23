90ca134b83d9e5632ddeb5068c68c159fa692a51
SeokYoon Kim <isdebrave@gmail.com>
2025-03-23 23:08:25 +0900

    :recycle: usecase로 매개변수가 아니라 repository를 받도록 수정

--- lib/domain/usecase/auth/post_dev_local_login_usecase.dart
+++ lib/domain/usecase/auth/post_dev_local_login_usecase.dart
@@ -1,33 +1,30 @@
-import 'package:dio/dio.dart';
-import 'package:frontend_mobile/core/resource/result/result.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend_mobile/core/resource/exception/custom_exception.dart';
+import 'package:frontend_mobile/core/resource/result.dart';
 import 'package:frontend_mobile/core/resource/usecase.dart';
-import 'package:frontend_mobile/data/request_body/auth/local_login_request_body.dart';
+import 'package:frontend_mobile/data/repository_impl/auth_repository_impl.dart';
 import 'package:frontend_mobile/domain/model/auth/local_login_model.dart';
+import 'package:frontend_mobile/domain/param/auth/local_login_params.dart';
 import 'package:frontend_mobile/domain/repository/auth_repository.dart';
 
-class PostDevLocalLoginUsecase extends Usecase<AuthRepository> {
-  /// [Dev 로그인(domain)](https://api.desserbee.com/swagger-ui/index.html#/Authentication/devlogin)
-  PostDevLocalLoginUsecase({required this.body});
+final Provider<PostDevLocalLoginUsecase> postDevLocalLoginUsecaseProvider =
+    Provider<PostDevLocalLoginUsecase>(
+      (Ref ref) => PostDevLocalLoginUsecase(
+        repository: ref.read(authRepositoryProvider),
+      ),
+    );
 
-  final LocalLoginRequestBody body;
+class PostDevLocalLoginUsecase
+    extends Usecase<LocalLoginModel, LocalLoginParams> {
+  /// [Dev 로그인(domain)](https://api.desserbee.com/swagger-ui/index.html#/Authentication/devlogin)
+  PostDevLocalLoginUsecase({required this.repository});
+
+  final AuthRepository repository;
 
   @override
-  Future<Result<LocalLoginModel>> call({
-    required AuthRepository repository,
+  Future<Result<LocalLoginModel, CustomException>> call({
+    required LocalLoginParams params,
   }) async {
-    try {
-      final LocalLoginModel response = await repository.postDevLocalLogin(
-        body: body,
-      );
-
-      return Result<LocalLoginModel>.success(response);
-    } catch (error) {
-      if (error is DioException) {
-        // TODO: 에러 경우의 수 나눠서 처리해야 함
-        return const Result<LocalLoginModel>.failure('dio 에러');
-      }
-
-      return const Result<LocalLoginModel>.failure('기타 에러');
-    }
+    return await repository.postDevLocalLogin(params: params);
   }
 }
