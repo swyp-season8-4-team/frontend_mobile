8708bc4fff30720103695538e4ede081cf334136
SeokYoon Kim <isdebrave@gmail.com>
2025-03-23 23:04:19 +0900

    :recycle: usecase 수정

--- lib/core/resource/usecase.dart
+++ lib/core/resource/usecase.dart
@@ -1,7 +1,15 @@
-import 'package:frontend_mobile/core/resource/model.dart';
-import 'package:frontend_mobile/core/resource/repository.dart';
-import 'package:frontend_mobile/core/resource/result/result.dart';
+import 'package:frontend_mobile/core/resource/exception/custom_exception.dart';
+import 'package:frontend_mobile/core/resource/result.dart';
 
-abstract class Usecase<T extends Repository> {
-  Future<Result<Model>> call({required T repository});
+/// T: 모델, P: 매개변수
+abstract class Usecase<T, P> {
+  Future<Result<T, CustomException>> call({required P params});
+
+  /// R1: 모델, R2: 매개변수
+  static Future<Result<T, CustomException>> execute<T, P>({
+    required Usecase<T, P> usecase,
+    required P params,
+  }) async {
+    return await usecase(params: params);
+  }
 }
