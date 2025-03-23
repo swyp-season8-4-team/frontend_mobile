a2b0a17786f9b15ae8969158715c9fda5d69ff4e
SeokYoon Kim <isdebrave@gmail.com>
2025-03-23 23:07:33 +0900

    :recycle: repository_impl에서 Result 객체를 반환하도록 수정

--- lib/data/repository_impl/auth_repository_impl.dart
+++ lib/data/repository_impl/auth_repository_impl.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend_mobile/core/resource/exception/api_call.dart';
+import 'package:frontend_mobile/core/resource/exception/custom_exception.dart';
+import 'package:frontend_mobile/core/resource/result.dart';
 import 'package:frontend_mobile/data/data_source/remote/auth_data_source.dart';
 import 'package:frontend_mobile/data/entity/auth/local_login_entity.dart';
 import 'package:frontend_mobile/data/mapper/auth_mapper.dart';
-import 'package:frontend_mobile/data/request_body/auth/local_login_request_body.dart';
 import 'package:frontend_mobile/domain/model/auth/local_login_model.dart';
+import 'package:frontend_mobile/domain/param/auth/local_login_params.dart';
 import 'package:frontend_mobile/domain/repository/auth_repository.dart';
 
 final Provider<AuthRepository> authRepositoryProvider =
@@ -17,11 +20,24 @@ class AuthRepositoryImpl implements AuthRepository {
   final AuthApi api;
 
   @override
-  Future<LocalLoginModel> postDevLocalLogin({
-    required LocalLoginRequestBody body,
+  Future<Result<LocalLoginModel, CustomException>> postDevLocalLogin({
+    required LocalLoginParams params,
   }) async {
-    final LocalLoginEntity response = await api.postDevLocalLogin(body: body);
+    final Result<LocalLoginEntity, CustomException> response = await apiCall(
+      api: () async => await api.postDevLocalLogin(body: params.toBody()),
+    );
 
-    return response.toModel();
+    return response.map(
+      success: (Success<LocalLoginEntity, CustomException> success) {
+        return Success<LocalLoginModel, CustomException>(
+          data: success.data.toModel(),
+        );
+      },
+      failure: (Failure<LocalLoginEntity, CustomException> failure) {
+        return Failure<LocalLoginModel, CustomException>(
+          exception: failure.exception,
+        );
+      },
+    );
   }
 }
