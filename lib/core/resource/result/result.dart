4bd70747ce2e3ac445a73e79a020f2ee83ddad99
SeokYoon Kim <isdebrave@gmail.com>
2025-03-23 22:59:03 +0900

    :fire: 사용하지 않는 파일 제거

--- lib/core/resource/result/result.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'result.freezed.dart';
-
-@freezed
-class Result<T> with _$Result<T> {
-  const factory Result.success(T data) = Success<T>;
-  const factory Result.failure(String message) = Failure<T>;
-}
