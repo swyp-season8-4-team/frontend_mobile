15c7d5059d741611417f4902db098bb1125542ad
SeokYoon Kim <isdebrave@gmail.com>
2025-03-26 00:55:39 +0900

    :wrench: scale color, semantic color 정의

---
+++ lib/common/design_system/foundation/color/scale_color_config.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ScaleColorConfig {
+  // Primary: Honey yellow
+  static Color primary10 = const Color(0xFF271900);
+  static Color primary80 = const Color(0xFFFFC858);
+  static Color primary90 = const Color(0xFFFFDEA7);
+  static Color primary100 = const Color(0xFFFFFFFF);
+
+  // Secondary: Olive brown
+  static Color secondary10 = const Color(0xFF251A03);
+  static Color secondary40 = const Color(0xFF6D5C3E);
+  static Color secondary90 = const Color(0xFFF7DFB9);
+  static Color secondary100 = const Color(0xFFFFFFFF);
+
+  // Success
+  static Color success10 = const Color(0xFF175A28);
+  static Color success50 = const Color(0xFF2FB350);
+  static Color success90 = const Color(0xFFEBF9EE);
+  static Color success100 = const Color(0xFFFFFFFF);
+
+  // Error
+  static Color error10 = const Color(0xFF7E0007);
+  static Color error40 = const Color(0xFFBA1A1A);
+  static Color error90 = const Color(0xFFFFDAD6);
+  static Color error100 = const Color(0xFFFFFFFF);
+
+  // Neutral
+  static Color neutral10 = const Color(0xFF14100C);
+  static Color neutral20 = const Color(0xFF3F3C39);
+  static Color neutral40 = const Color(0xFF9693BE);
+  static Color neutral50 = const Color(0xFFCDC8C3);
+  static Color neutral70 = const Color(0xFFEFEDEB);
+  static Color neutral80 = const Color(0xFFF5F2F0);
+
+  // Surface
+  static Color surface50 = const Color.fromRGBO(94, 66, 0, 0.14);
+  static Color surface60 = const Color.fromRGBO(94, 66, 0, 0.11);
+  static Color surface70 = const Color.fromRGBO(94, 66, 0, 0.08);
+  static Color surface80 = const Color.fromRGBO(94, 66, 0, 0.05);
+  static Color surface90 = const Color(0xFFFAFAFA);
+}
---
+++ lib/common/design_system/foundation/color/semantic_color_config.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+
+class SemanticColorConfig {
+  // Primary: Honey yellow
+  static Color primary = ScaleColorConfig.primary80;
+  static Color onPrimary = ScaleColorConfig.primary100;
+  static Color primaryContainer = ScaleColorConfig.primary90;
+  static Color onPrimaryContainer = ScaleColorConfig.primary10;
+
+  // Secondary: Olive brown
+  static Color secondary = ScaleColorConfig.secondary40;
+  static Color onSecondary = ScaleColorConfig.secondary100;
+  static Color secondaryContainer = ScaleColorConfig.secondary90;
+  static Color onSecondaryContainer = ScaleColorConfig.secondary10;
+
+  // Success
+  static Color success = ScaleColorConfig.success50;
+  static Color onSuccess = ScaleColorConfig.success100;
+  static Color successContainer = ScaleColorConfig.success90;
+  static Color onSuccessContainer = ScaleColorConfig.success10;
+
+  // Error
+  static Color error = ScaleColorConfig.error40;
+  static Color onError = ScaleColorConfig.error100;
+  static Color errorContainer = ScaleColorConfig.error90;
+  static Color onErrorContainer = ScaleColorConfig.error10;
+
+  // Surface
+  static Color surface = ScaleColorConfig.surface90;
+  static Color onSurface = ScaleColorConfig.neutral10;
+  static Color onSurfaceVarient = ScaleColorConfig.neutral40;
+  static Color surfaceContainerLowest = ScaleColorConfig.surface90;
+  static Color surfaceContainerLow = ScaleColorConfig.surface80;
+  static Color surfaceContainer = ScaleColorConfig.surface70;
+  static Color surfaceContainerHigh = ScaleColorConfig.surface60;
+  static Color surfaceContainerHighest = ScaleColorConfig.surface50;
+
+  // Inverse
+  static Color inverseSurface = ScaleColorConfig.neutral20;
+  static Color onInverseSurface = ScaleColorConfig.neutral80;
+  static Color inversePrimary = ScaleColorConfig.primary80;
+
+  // Outline
+  static Color outline = ScaleColorConfig.neutral50;
+  static Color outlineVarient = ScaleColorConfig.neutral70;
+}
