c111b0c71bd5299ccdb6938d92b223ee739b6df1
SeokYoon Kim <isdebrave@gmail.com>
2025-03-26 23:55:21 +0900

    :memo: 추가하지 않은 scale 컬러 반영

--- lib/common/design_system/foundation/color/scale_color_config.dart
+++ lib/common/design_system/foundation/color/scale_color_config.dart
@@ -2,36 +2,70 @@ import 'package:flutter/material.dart';
 
 class ScaleColorConfig {
   // Primary: Honey yellow
+  static Color primary5 = const Color(0xFF190F00);
   static Color primary10 = const Color(0xFF271900);
+  static Color primary20 = const Color(0xFF412D00);
+  static Color primary30 = const Color(0xFF5E4200);
+  static Color primary40 = const Color(0xFF7C5800);
+  static Color primary50 = const Color(0xFF9B6F00);
+  static Color primary60 = const Color(0xFFBC8701);
+  static Color primary70 = const Color(0xFFDAA227);
   static Color primary80 = const Color(0xFFFFC858);
   static Color primary90 = const Color(0xFFFFDEA7);
   static Color primary100 = const Color(0xFFFFFFFF);
 
   // Secondary: Olive brown
+  static Color secondary5 = const Color(0xFF190F00);
   static Color secondary10 = const Color(0xFF251A03);
+  static Color secondary20 = const Color(0xFF3C2E14);
+  static Color secondary30 = const Color(0xFF544429);
   static Color secondary40 = const Color(0xFF6D5C3E);
+  static Color secondary50 = const Color(0xFF877455);
+  static Color secondary60 = const Color(0xFFA28E6D);
+  static Color secondary70 = const Color(0xFFBEA885);
+  static Color secondary80 = const Color(0xFFDAC39F);
   static Color secondary90 = const Color(0xFFF7DFB9);
   static Color secondary100 = const Color(0xFFFFFFFF);
 
   // Success
+  static Color success5 = const Color(0xFF0D3216);
   static Color success10 = const Color(0xFF175A28);
+  static Color success20 = const Color(0xFF1D6D31);
+  static Color success30 = const Color(0xFF1F7735);
+  static Color success40 = const Color(0xFF248B3E);
   static Color success50 = const Color(0xFF2FB350);
+  static Color success60 = const Color(0xFF3ECA61);
+  static Color success70 = const Color(0xFF76D98F);
+  static Color success80 = const Color(0xFFD6F4DE);
   static Color success90 = const Color(0xFFEBF9EE);
   static Color success100 = const Color(0xFFFFFFFF);
 
   // Error
+  static Color error5 = const Color(0xFF540003);
   static Color error10 = const Color(0xFF7E0007);
+  static Color error20 = const Color(0xFF93000A);
+  static Color error30 = const Color(0xFFA80719);
   static Color error40 = const Color(0xFFBA1A1A);
+  static Color error50 = const Color(0xFFDE3730);
+  static Color error60 = const Color(0xFFFF5449);
+  static Color error70 = const Color(0xFFFF897D);
+  static Color error80 = const Color(0xFFFFB4AB);
   static Color error90 = const Color(0xFFFFDAD6);
   static Color error100 = const Color(0xFFFFFFFF);
 
   // Neutral
+  static Color neutral0 = const Color(0xFF000000);
+  static Color neutral5 = const Color(0xFF0F0C0C);
   static Color neutral10 = const Color(0xFF14100C);
   static Color neutral20 = const Color(0xFF3F3C39);
+  static Color neutral30 = const Color(0xFF635F59);
   static Color neutral40 = const Color(0xFF9693BE);
   static Color neutral50 = const Color(0xFFCDC8C3);
+  static Color neutral60 = const Color(0xFFE9E4E0);
   static Color neutral70 = const Color(0xFFEFEDEB);
   static Color neutral80 = const Color(0xFFF5F2F0);
+  static Color neutral90 = const Color(0xFFFCFAF8);
+  static Color neutral100 = const Color(0xFFFFFFFF);
 
   // Surface
   static Color surface50 = const Color.fromRGBO(94, 66, 0, 0.14);
