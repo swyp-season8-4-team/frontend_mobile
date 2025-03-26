0219a2c276eb15641afa8c294eab477bf67b6220
generalChang <rhdrnsckdgh0921@naver.com>
2025-03-27 01:26:21 +0900

    :lipstick: CustomEtcTag 추가

--- lib/common/design_system/component/tag/etc_tag.dart
+++ lib/common/design_system/component/tag/etc_tag.dart
@@ -1,19 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/foundation/font_weight/font_weight_config.dart';
 import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
 
-class EtcTag extends StatelessWidget {
-  const EtcTag({
+class CustomEtcTag extends StatelessWidget {
+  const CustomEtcTag({
     required this.label,
-    required this.onTap,
     required this.icon,
+    this.onTap,
     super.key,
   });
   final String label;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
   final SvgGenImage icon;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    /// TODO: 타이포그래피 내 텍스트스타일 적용 필요
+    const TextStyle baseTextStyle = TextStyle(
+      fontSize: 11,
+      height: 14 / 11,
+      fontWeight: FontWeightConfig.regular,
+      color: Color(0xFF393939),
+    );
+    return GestureDetector(
+      onTap: onTap,
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6),
+
+          /// TODO: 컬러 시스템값 적용 필요
+          color: const Color(0xFFF5F5F5),
+        ),
+        height: 32,
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 2.5),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            icon.svg(),
+            const SizedBox(width: 10),
+            Text(label, style: baseTextStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// 주차 가능 태그
+class CanParkingTag extends StatelessWidget {
+  const CanParkingTag({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomEtcTag(label: '주차 가능', icon: Assets.icon.etc.a23x16CarFilled);
+  }
+}
+
+/// 반려동물 동반 태그
+class PetTag extends StatelessWidget {
+  const PetTag({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomEtcTag(
+      label: '반려동물 동반',
+      icon: Assets.icon.etc.a16x16PetFilled,
+    );
+  }
+}
+
+/// 텀블러 할인 태그
+class DiscountTumblerTag extends StatelessWidget {
+  const DiscountTumblerTag({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomEtcTag(
+      label: '텀블러 할인',
+      icon: Assets.icon.etc.a13x20TumblerFilled,
+    );
   }
 }
