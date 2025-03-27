0c8f28edbb35d9d9981a76d0874375e3b3e786ec
generalChang <rhdrnsckdgh0921@naver.com>
2025-03-28 00:52:46 +0900

    :lipstick: 버튼형 인풋 박스 (CustomInputBox.picker) 모듈화

--- lib/common/design_system/component/textfield/input_box.dart
+++ lib/common/design_system/component/textfield/input_box.dart
@@ -4,8 +4,24 @@ import 'package:frontend_mobile/common/design_system/foundation/color/scale_colo
 import 'package:frontend_mobile/common/design_system/foundation/font_weight/font_weight_config.dart';
 import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
 
-/// Input Box
+/// Text Field
 /// https://www.figma.com/design/Cmw8GLJYfuUVf9A3QNxqgW/SWYP_%EC%95%B1_1%EA%B8%B0_%EB%94%94%EC%A0%80%EB%B9%84?node-id=400-73177&t=3ArDzZQuJj0XGk5f-4
+
+enum InputBoxType {
+  // 사용자가 직접 텍스트를 입력할 수 있는 input (default)
+  direct,
+
+  // 사용자가 직접 텍스트를 입력하지 않는 버튼형 input
+  picker,
+}
+
+/// Input Box
+/// ---------------------------
+/// Input Box를 사용하고 싶은 경우
+/// CustomInputBox를 호출
+///
+/// 버튼형 Input Box를 사용하고 싶은 경우
+/// CustomInputBox.picker를 호출
 class CustomInputBox extends StatefulWidget {
   const CustomInputBox({
     this.label,
@@ -23,29 +39,54 @@ class CustomInputBox extends StatefulWidget {
     this.success = false,
     this.onCloseButtonTap,
     this.onVisibilityButtonTap,
-  });
+    this.onTap,
+  }) : type = InputBoxType.direct;
+
+  const CustomInputBox.picker({
+    this.label,
+    super.key,
+    this.controller,
+    this.hintText,
+    this.onTap,
+  }) : visibility = true,
+       visibilityControll = false,
+       errorText = null,
+       successText = null,
+       onChanged = null,
+       inputFormatters = null,
+       keyboardType = null,
+       error = false,
+       success = false,
+       onCloseButtonTap = null,
+       onVisibilityButtonTap = null,
+       type = InputBoxType.picker;
+  // 라벨
   final String? label;
-
-  /// 글자 암호화 컨트롤 표시 여부
+  // 글자 암호화 컨트롤 표시 여부
   final bool visibilityControll;
-
-  /// 글자 암호화 여부
+  // 글자 암호화 여부
   final bool visibility;
   final void Function(String)? onChanged;
   final TextEditingController? controller;
+  // 힌트 텍스트
   final String? hintText;
   final List<TextInputFormatter>? inputFormatters;
   final TextInputType? keyboardType;
-
-  /// 에러 여부
+  // 인풋 박스 탭 이벤트 콜백
+  final VoidCallback? onTap;
+  // 에러 여부
   final bool error;
   final String? errorText;
-
-  /// 성공 여부
+  // 성공 여부
   final bool success;
   final String? successText;
+  // 지우기 이벤트 탭 이벤트 콜백
   final VoidCallback? onCloseButtonTap;
+  // 텍스트 암호화 탭 이벤트 이벤트 콜백
   final VoidCallback? onVisibilityButtonTap;
+  // input box type
+  // default : direct
+  final InputBoxType type;
   @override
   State<CustomInputBox> createState() => _CustomInputBoxState();
 }
@@ -80,114 +121,128 @@ class _CustomInputBoxState extends State<CustomInputBox> {
       letterSpacing: -0.21,
     );
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        if (widget.label != null)
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: Text(widget.label!, style: labelTextStyle),
-          ),
-        TextField(
-          onChanged: widget.onChanged,
-          onTapOutside: (_) {
-            // 바깥 영역 터치시
-            // 자동으로 unfocus
-            FocusManager.instance.primaryFocus?.unfocus();
-          },
-          controller: _textEditingController,
-          inputFormatters: widget.inputFormatters,
-          keyboardType: widget.keyboardType,
-          style: textTheme.bodyMedium?.copyWith(
-            color: ScaleColorConfig.neutral20,
-          ),
-          obscureText: !widget.visibility,
-          // TODO: 컬러 시스템 값 적용 필요
-          cursorColor: const Color(0xFF393939),
-          cursorWidth: 1,
-          decoration: InputDecoration(
-            filled: true,
-            isDense: true,
-            error: widget.error ? const SizedBox.shrink() : null,
-            fillColor: ScaleColorConfig.neutral90,
-            suffixIcon: Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  _buildCloseButton(),
-                  _buildVisibilityOptionButton(),
-                ],
+    return GestureDetector(
+      onTap: widget.onTap,
+      behavior: HitTestBehavior.translucent,
+      child: IgnorePointer(
+        ignoring: widget.type == InputBoxType.picker,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            if (widget.label != null)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(widget.label!, style: labelTextStyle),
+              ),
+            TextField(
+              onChanged: widget.onChanged,
+              onTapOutside: (_) {
+                // 바깥 영역 터치시
+                // 자동으로 unfocus
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              readOnly: widget.type == InputBoxType.picker,
+              controller: _textEditingController,
+              inputFormatters: widget.inputFormatters,
+              keyboardType: widget.keyboardType,
+              style: textTheme.bodyMedium?.copyWith(
+                color: ScaleColorConfig.neutral20,
+              ),
+              obscureText: !widget.visibility,
+              // TODO: 컬러 시스템 값 적용 필요
+              cursorColor: const Color(0xFF393939),
+              cursorWidth: 1,
+              decoration: InputDecoration(
+                filled: true,
+                isDense: true,
+                error: widget.error ? const SizedBox.shrink() : null,
+                fillColor:
+                    widget.type == InputBoxType.picker
+                        ? ScaleColorConfig.neutral70
+                        : ScaleColorConfig.neutral90,
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child:
+                      widget.type == InputBoxType.picker
+                          ? Assets.icon.arrow.a18RightLine.svg()
+                          : Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              _buildCloseButton(),
+                              _buildVisibilityOptionButton(),
+                            ],
+                          ),
+                ),
+                suffixIconConstraints: const BoxConstraints(
+                  // 아이콘 너비 + 왼쪽 패딩
+                  minWidth: 18 + 16,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 13.5,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: BorderSide(color: colorScheme.outline),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: BorderSide(color: colorScheme.outline),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: BorderSide(color: ScaleColorConfig.neutral30),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: BorderSide(color: colorScheme.error),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                  borderSide: BorderSide(color: colorScheme.error),
+                ),
+                hintText: widget.hintText,
+                hintStyle: textTheme.bodyMedium?.copyWith(
+                  color: ScaleColorConfig.neutral50,
+                ),
               ),
             ),
-            suffixIconConstraints: const BoxConstraints(
-              // 아이콘 너비 + 왼쪽 패딩
-              minWidth: 18 + 16,
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 13.5,
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: BorderSide(color: colorScheme.outline),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: BorderSide(color: colorScheme.outline),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: BorderSide(color: ScaleColorConfig.neutral30),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: BorderSide(color: colorScheme.error),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(6),
-              borderSide: BorderSide(color: colorScheme.error),
-            ),
-            hintText: widget.hintText,
-            hintStyle: textTheme.bodyMedium?.copyWith(
-              color: ScaleColorConfig.neutral50,
-            ),
-          ),
-        ),
-        if (widget.error && widget.errorText != null)
-          Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: Row(
-              children: <Widget>[
-                Assets.icon.system.a16WarningCircleFilled.svg(),
-                const SizedBox(width: 5),
-                Expanded(
-                  child: Text(widget.errorText!, style: feedbackTextStyle),
-                ),
-              ],
-            ),
-          )
-        else if (widget.success && widget.successText != null)
-          Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: Row(
-              children: <Widget>[
-                Assets.icon.system.a16CheckCircleFilled.svg(),
-                const SizedBox(width: 5),
-
-                Expanded(
-                  child: Text(
-                    widget.successText!,
-                    style: feedbackTextStyle.copyWith(
-                      color: ScaleColorConfig.success60,
+            if (widget.error && widget.errorText != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: Row(
+                  children: <Widget>[
+                    Assets.icon.system.a16WarningCircleFilled.svg(),
+                    const SizedBox(width: 5),
+                    Expanded(
+                      child: Text(widget.errorText!, style: feedbackTextStyle),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-      ],
+              )
+            else if (widget.success && widget.successText != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: Row(
+                  children: <Widget>[
+                    Assets.icon.system.a16CheckCircleFilled.svg(),
+                    const SizedBox(width: 5),
+
+                    Expanded(
+                      child: Text(
+                        widget.successText!,
+                        style: feedbackTextStyle.copyWith(
+                          color: ScaleColorConfig.success60,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 
