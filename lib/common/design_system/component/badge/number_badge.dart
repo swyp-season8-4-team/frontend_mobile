a364201fe9244809a735767c3f784f3001d7f2a9
SeokYoon Kim <isdebrave@gmail.com>
2025-03-29 20:21:49 +0900

    :lipstick: 숫자 뱃지 생성

---
+++ lib/common/design_system/component/badge/number_badge.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+
+class CustomNumberBadge extends StatelessWidget {
+  const CustomNumberBadge({required this.number, super.key});
+
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return FittedBox(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(99),
+          color: ScaleColorConfig.error90,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+        child: Align(
+          child: Text(
+            number >= 1000 ? '999+' : number.toString(),
+            style: textTheme.labelSmall?.copyWith(
+              color: ScaleColorConfig.error10,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
