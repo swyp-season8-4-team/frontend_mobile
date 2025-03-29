e979cb587862d1942a36d7724309d146dfd5a9db
SeokYoon Kim <isdebrave@gmail.com>
2025-03-29 20:21:33 +0900

    :lipstick: N 뱃지 생성

---
+++ lib/common/design_system/component/badge/new_badge.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+
+class CustomNewBadge extends StatelessWidget {
+  const CustomNewBadge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: ScaleColorConfig.error40,
+      ),
+      child: Align(
+        child: Text(
+          'N',
+          style: textTheme.labelSmall?.copyWith(
+            color: ScaleColorConfig.primary100,
+          ),
+        ),
+      ),
+    );
+  }
+}
