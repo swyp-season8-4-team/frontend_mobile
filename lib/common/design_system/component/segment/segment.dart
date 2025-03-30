7b80cdd38207ad536f544cc394f031c81c6c9535
SeokYoon Kim <isdebrave@gmail.com>
2025-03-30 23:09:26 +0900

    :fire: 주석 코드 제거

--- lib/common/design_system/component/segment/segment.dart
+++ lib/common/design_system/component/segment/segment.dart
@@ -27,7 +27,6 @@ class CustomSegment extends StatelessWidget {
                 labelPadding: const EdgeInsets.symmetric(
                   horizontal: 20,
                   vertical: 8,
-                  // vertical: 18,
                 ),
                 dividerColor: Colors.transparent,
                 indicator: BoxDecoration(
