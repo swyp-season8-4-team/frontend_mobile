77c37e89059d8d473188221469cd44f973726010
generalChang <rhdrnsckdgh0921@naver.com>
2025-03-30 15:20:47 +0900

    :lipstick: CustomSearch 추가

---
+++ lib/common/design_system/component/search/search.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
+
+class CustomSearch extends StatefulWidget {
+  const CustomSearch({
+    this.onChanged,
+    this.controller,
+    this.hintText = '검색어를 입력해주세요',
+    this.inputFormatters,
+    this.keyboardType,
+    this.onCloseButtonTap,
+    super.key,
+  });
+
+  final void Function(String)? onChanged;
+  final TextEditingController? controller;
+  // 힌트 텍스트
+  final String hintText;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextInputType? keyboardType;
+  // 지우기 이벤트 탭 이벤트 콜백
+  final VoidCallback? onCloseButtonTap;
+
+  @override
+  State<CustomSearch> createState() => _CustomSearchState();
+}
+
+class _CustomSearchState extends State<CustomSearch> {
+  late final TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = widget.controller ?? TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return TextField(
+      onChanged: widget.onChanged,
+      onTapOutside: (_) {
+        // 바깥 영역 터치시
+        // 자동으로 unfocus
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      controller: _textEditingController,
+      inputFormatters: widget.inputFormatters,
+      keyboardType: widget.keyboardType,
+      // TODO: 컬러 시스템 값 적용 필요
+      style: textTheme.bodyMedium?.copyWith(color: const Color(0xFF393939)),
+      // TODO: 컬러 시스템 값 적용 필요
+      cursorColor: const Color(0xFF393939),
+      cursorHeight: 17,
+      cursorWidth: 1,
+      decoration: InputDecoration(
+        filled: true,
+        isDense: true,
+        fillColor: ScaleColorConfig.neutral90,
+        suffixIcon: Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () {
+              widget.onCloseButtonTap?.call();
+              _textEditingController.clear();
+            },
+            child: Assets.icon.etc.a18CloseCircleFilledEnabled.svg(),
+          ),
+        ),
+        suffixIconConstraints: const BoxConstraints(
+          // 아이콘 너비 + 우측 패딩
+          minWidth: 18 + 16,
+        ),
+        prefixIcon: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 10),
+          child: Assets.icon.file.search1Line.svg(
+            colorFilter: ColorFilter.mode(
+              ScaleColorConfig.neutral40,
+              BlendMode.srcIn,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        prefixIconConstraints: const BoxConstraints(
+          // 아이콘 너비 + 좌측 패딩
+          minWidth: 24 + 16,
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 12.5),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: BorderSide(color: colorScheme.outline),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: BorderSide(color: colorScheme.outline),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: BorderSide(color: ScaleColorConfig.neutral30),
+        ),
+        hintText: widget.hintText,
+        hintStyle: textTheme.bodyMedium?.copyWith(
+          color: ScaleColorConfig.neutral50,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // 내부에서 생성한 컨트롤러인 경우, 내부에서 dispose
+    if (_textEditingController != widget.controller) {
+      _textEditingController.dispose();
+    }
+    super.dispose();
+  }
+}
