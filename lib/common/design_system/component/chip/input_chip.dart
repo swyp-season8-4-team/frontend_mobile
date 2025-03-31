9fe3b0429f54b5a9db78fee0c67a90126e76e8ec
SeokYoon Kim <isdebrave@gmail.com>
2025-04-01 02:54:56 +0900

    :recycle: input을 CustomChip 기반으로 생성

--- lib/common/design_system/component/chip/input_chip.dart
+++ lib/common/design_system/component/chip/input_chip.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:frontend_mobile/common/design_system/component/chip/custom_chip.dart';
 import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
 import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
 
@@ -20,12 +21,8 @@ class CustomInputChip extends StatefulWidget {
 class _CustomInputChipState extends State<CustomInputChip> {
   Color _iconColor = ScaleColorConfig.neutral50;
 
-  @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-
-    return RawChip(
-      color: WidgetStateProperty.resolveWith((Set<WidgetState> states) {
+  WidgetStateProperty<Color?> get _color =>
+      WidgetStateProperty.resolveWith((Set<WidgetState> states) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
           /// Pressed
           if (states.contains(WidgetState.pressed)) {
@@ -42,31 +39,20 @@ class _CustomInputChipState extends State<CustomInputChip> {
         });
 
         return ScaleColorConfig.primary100;
-      }),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24),
-        side: BorderSide(color: ScaleColorConfig.neutral50),
-      ),
-      label: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(widget.label),
-          const SizedBox(width: 4),
-          SvgPicture.asset(
-            Assets.icon.system.closeCircleFill.path,
-            width: 16,
-            colorFilter: ColorFilter.mode(_iconColor, BlendMode.srcIn),
-          ),
-        ],
-      ),
-      labelStyle: textTheme.labelLarge?.copyWith(
-        color: ScaleColorConfig.primary20,
-      ),
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomChip(
+      color: _color,
+      label: widget.label,
       onPressed: widget.onPressed,
-      visualDensity: const VisualDensity(vertical: -4),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      labelPadding: EdgeInsets.zero,
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      side: const BorderSide(color: ScaleColorConfig.neutral50),
+      trailing: SvgPicture.asset(
+        Assets.icon.system.closeCircleFill.path,
+        width: 16,
+        colorFilter: ColorFilter.mode(_iconColor, BlendMode.srcIn),
+      ),
     );
   }
 }
