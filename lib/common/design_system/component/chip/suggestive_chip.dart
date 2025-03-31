c3aeeed3f3930673c9799ae12ac872ddf222724f
SeokYoon Kim <isdebrave@gmail.com>
2025-04-01 02:55:13 +0900

    :recycle: suggestive를 CustomChip 기반으로 생성

--- lib/common/design_system/component/chip/suggestive_chip.dart
+++ lib/common/design_system/component/chip/suggestive_chip.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/component/chip/custom_chip.dart';
 import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
 
 class CustomSuggestiveChip extends StatefulWidget {
   const CustomSuggestiveChip({
     required this.label,
     required this.onPressed,
-    required this.number,
+    this.number,
     super.key,
   });
 
   final String label;
   final VoidCallback? onPressed;
-  final int number;
+  final int? number;
 
   @override
   State<CustomSuggestiveChip> createState() => _CustomSuggestiveChipState();
@@ -49,38 +50,25 @@ class _CustomSuggestiveChipState extends State<CustomSuggestiveChip> {
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
 
-    return RawChip(
+    return CustomChip(
       color: _color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24),
-        side: BorderSide(
-          color:
-              !_isSelected
-                  ? ScaleColorConfig.neutral50
-                  : ScaleColorConfig.primary70,
-        ),
-      ),
-      label: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(widget.label),
-          const SizedBox(width: 4),
-          Text(
-            widget.number >= 1000 ? '999+' : widget.number.toString(),
-            style: textTheme.labelMedium?.copyWith(
-              color: ScaleColorConfig.success40,
-            ),
-          ),
-        ],
-      ),
-      labelStyle: textTheme.labelLarge?.copyWith(
-        color: ScaleColorConfig.primary20,
-      ),
+      label: widget.label,
       onPressed: _onPressed,
-      visualDensity: const VisualDensity(vertical: -4),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      labelPadding: EdgeInsets.zero,
-      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      side: BorderSide(
+        color:
+            !_isSelected
+                ? ScaleColorConfig.neutral50
+                : ScaleColorConfig.primary70,
+      ),
+      trailing:
+          widget.number != null
+              ? Text(
+                widget.number! >= 1000 ? '999+' : widget.number.toString(),
+                style: textTheme.labelMedium?.copyWith(
+                  color: ScaleColorConfig.success40,
+                ),
+              )
+              : null,
     );
   }
 }
