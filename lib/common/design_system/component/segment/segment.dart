427582b0982087877c03921bcff1b4b7523bfd19
SeokYoon Kim <isdebrave@gmail.com>
2025-04-01 00:32:39 +0900

    :recycle: SegmentType 정의 및 tabBarView 적용

--- lib/common/design_system/component/segment/segment.dart
+++ lib/common/design_system/component/segment/segment.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
 
+class SegmentType {
+  SegmentType({required this.tab, required this.child});
+
+  /// TabBar 안에 들어가는 텍스트
+  final String tab;
+
+  /// TabBarView에 들어가는 위젯
+  final Widget child;
+}
+
 class CustomSegment extends StatelessWidget {
   const CustomSegment({required this.first, required this.second, super.key});
 
-  final String first;
-  final String second;
+  final SegmentType first;
+  final SegmentType second;
 
   BorderRadius get _borderRadius => BorderRadius.circular(6);
+
+  Widget _tabBar({required TextTheme textTheme}) {
+    return ClipRRect(
+      borderRadius: _borderRadius,
+      child: Material(
+        color: ScaleColorConfig.neutral60,
+        child: SizedBox(
+          width: 208,
+          child: IntrinsicHeight(
+            child: TabBar(
+              indicatorSize: TabBarIndicatorSize.tab,
+              labelPadding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 8,
+              ),
+              dividerColor: Colors.transparent,
+              indicator: BoxDecoration(
+                color: ScaleColorConfig.secondary40,
+                borderRadius: _borderRadius,
+              ),
+              labelStyle: textTheme.labelLarge?.copyWith(
+                color: ScaleColorConfig.neutral100,
+              ),
+              unselectedLabelStyle: textTheme.labelLarge?.copyWith(
+                color: ScaleColorConfig.neutral10,
+              ),
+              overlayColor: WidgetStateColor.resolveWith((
+                Set<WidgetState> states,
+              ) {
+                return ScaleColorConfig.neutral50;
+              }),
+              splashBorderRadius: _borderRadius,
+              tabs: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[Text(first.tab)],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[Text(second.tab)],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _tabBarView() {
+    return Expanded(
+      child: TabBarView(children: <Widget>[first.child, second.child]),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,50 +79,8 @@ class CustomSegment extends StatelessWidget {
 
     return DefaultTabController(
       length: 2,
-      child: ClipRRect(
-        borderRadius: _borderRadius,
-        child: Material(
-          color: ScaleColorConfig.neutral60,
-          child: SizedBox(
-            width: 208,
-            child: IntrinsicHeight(
-              child: TabBar(
-                indicatorSize: TabBarIndicatorSize.tab,
-                labelPadding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 8,
-                ),
-                dividerColor: Colors.transparent,
-                indicator: BoxDecoration(
-                  color: ScaleColorConfig.secondary40,
-                  borderRadius: _borderRadius,
-                ),
-                labelStyle: textTheme.labelLarge?.copyWith(
-                  color: ScaleColorConfig.neutral100,
-                ),
-                unselectedLabelStyle: textTheme.labelLarge?.copyWith(
-                  color: ScaleColorConfig.neutral10,
-                ),
-                overlayColor: WidgetStateColor.resolveWith((
-                  Set<WidgetState> states,
-                ) {
-                  return ScaleColorConfig.neutral50;
-                }),
-                splashBorderRadius: _borderRadius,
-                tabs: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[Text(first)],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[Text(second)],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+      child: Column(
+        children: <Widget>[_tabBar(textTheme: textTheme), _tabBarView()],
       ),
     );
   }
