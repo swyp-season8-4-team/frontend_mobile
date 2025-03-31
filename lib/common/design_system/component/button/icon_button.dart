667017f36fd5370ebe6c91265a039cf14c9e54e0
SeokYoon Kim <isdebrave@gmail.com>
2025-04-01 05:25:15 +0900

    :recycle: icon 버튼 수정

--- lib/common/design_system/component/button/icon_button.dart
+++ lib/common/design_system/component/button/icon_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+import 'package:frontend_mobile/common/design_system/foundation/shadow/shadow_config.dart';
 import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
 
 class CustomIconButton extends StatefulWidget {
@@ -20,7 +21,7 @@ class CustomIconButton extends StatefulWidget {
 }
 
 class _CustomIconButtonState extends State<CustomIconButton> {
-  Color _iconColor = ScaleColorConfig.black;
+  Color _iconColor = Colors.black;
   bool _isSelected = false;
 
   WidgetStateProperty<Color?>? get _foregroundColor =>
@@ -92,24 +93,33 @@ class _CustomIconButtonState extends State<CustomIconButton> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Shadow 정의되면 적용해야 함
-    return IconButton(
-      onPressed: !widget.disabled ? _onPressed : null,
-      style: IconButton.styleFrom(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(99)),
-        minimumSize: Size.zero,
-        padding: const EdgeInsets.all(4),
-        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        elevation: 0,
-      ).copyWith(
-        foregroundColor: _foregroundColor,
-        backgroundColor: _backgroundColor,
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: ShadowConfig().level1,
+        shape: BoxShape.circle,
       ),
-      icon: SvgPicture.asset(
-        widget.svg.path,
-        colorFilter: ColorFilter.mode(_iconColor, BlendMode.srcIn),
+      child: IconButton(
+        onPressed: !widget.disabled ? _onPressed : null,
+        style: IconButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(99),
+          ),
+          minimumSize: Size.zero,
+          padding: const EdgeInsets.all(4),
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          elevation: 0,
+        ).copyWith(
+          foregroundColor: _foregroundColor,
+          backgroundColor: _backgroundColor,
+        ),
+        icon: Padding(
+          padding: const EdgeInsets.all(4),
+          child: SvgPicture.asset(
+            widget.svg.path,
+            colorFilter: ColorFilter.mode(_iconColor, BlendMode.srcIn),
+          ),
+        ),
       ),
-      // icon: const Icon(Icons.abc),
     );
   }
 }
