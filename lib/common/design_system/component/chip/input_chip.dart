bba8f4351e531fbc59e80717295d8a8af00d1e79
SeokYoon Kim <isdebrave@gmail.com>
2025-04-01 23:33:02 +0900

    :recycle: closeCircleFill 구현 방식 수정

--- lib/common/design_system/component/chip/input_chip.dart
+++ lib/common/design_system/component/chip/input_chip.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:frontend_mobile/common/design_system/component/chip/custom_chip.dart';
 import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
 import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
@@ -48,8 +47,7 @@ class _CustomInputChipState extends State<CustomInputChip> {
       label: widget.label,
       onPressed: widget.onPressed,
       side: const BorderSide(color: ScaleColorConfig.neutral50),
-      trailing: SvgPicture.asset(
-        Assets.icon.system.closeCircleFill.path,
+      trailing: Assets.icon.system.closeCircleFill.svg(
         width: 16,
         colorFilter: ColorFilter.mode(_iconColor, BlendMode.srcIn),
       ),
