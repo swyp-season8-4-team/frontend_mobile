6a576f4f5e9e3825a7fc7808c41a958da536b7c3
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-07 16:20:19 +0900

    :sparkles: 라우팅 설계에 사용될 자료구조 추가

---
+++ lib/core/resource/route_info.dart
@@ -0,0 +1,10 @@
+/// 라우팅 설계에 필요한 자로구조
+class RouteInfo {
+  const RouteInfo({required this.name, required this.path});
+
+  // route name
+  final String name;
+
+  // route path
+  final String path;
+}
