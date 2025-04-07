a3a7ed0ca2c2b9c5b1e26bcde0f05bc1bb0597d5
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-07 16:32:34 +0900

    :sparkles: 앱 전반에 걸쳐 사용할 Route 정보 관리 모듈 추가

---
+++ lib/presentation/router/routes.dart
@@ -0,0 +1,16 @@
+import 'package:frontend_mobile/core/resource/route_info.dart';
+
+/// 앱에서 전반적으로 사용될 스크린 위젯(페이지) 경로(Route) 정보 정의
+/// 경로정보들을 static 필드로 미리 정의한 후, 라우팅 구현 및 화면 전환 시 사용
+abstract class AppRoutes {
+  AppRoutes._();
+
+  // 인증 관련 페이지의 기반 Route Info
+  static const RouteInfo auth = RouteInfo(name: '/auth', path: '/auth');
+
+  // 로그인 화면
+  static final RouteInfo localLogin = RouteInfo(
+    name: '${auth.name}/local_login',
+    path: 'local_login',
+  );
+}
