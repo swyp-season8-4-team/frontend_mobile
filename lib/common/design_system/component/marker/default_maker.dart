f1b6cdb19d9fc95344588dfe62efe7b04f6190a5
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-08 00:33:14 +0900

    :lipstick: CustomDefaultMarker 추가

---
+++ lib/common/design_system/component/marker/default_maker.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
+
+/// Default Marker
+/// https://www.figma.com/design/S1zkOn7DjDJ0b1mcPVJRil/SWYP_%E1%84%8B%E1%85%A2%E1%86%B8_1%E1%84%80%E1%85%B5_%E1%84%83%E1%85%B5%E1%84%8C%E1%85%A5%E1%84%87%E1%85%B5?node-id=40000687-133098&m=dev
+class CustomDefaultMarker extends StatelessWidget {
+  const CustomDefaultMarker({required this.text, super.key});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: <Widget>[
+        Positioned(
+          top: 25,
+          child: Container(
+            alignment: Alignment.center,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                border: Border.all(color: ScaleColorConfig.primary30),
+                color: ScaleColorConfig.primary100,
+              ),
+              constraints: const BoxConstraints(minWidth: 38, maxWidth: 100),
+              padding: const EdgeInsets.all(4),
+              child: Text(
+                text,
+                style: textTheme.labelSmall?.copyWith(
+                  color: ScaleColorConfig.primary20,
+                ),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ),
+          ),
+        ),
+        Assets.icon.map.a22x30Marker1.svg(),
+      ],
+    );
+  }
+}
