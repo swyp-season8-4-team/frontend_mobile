9250844b1fb8ad6c076a4551339250fa5b383967
SeokYoon Kim <isdebrave@gmail.com>
2025-04-08 23:37:59 +0900

    :recycle: password validation 체리픽

--- lib/core/resource/extension.dart
+++ lib/core/resource/extension.dart
@@ -1,4 +1,8 @@
 extension StringValidationExt on String {
   bool get isEmail =>
       RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(this);
+
+  bool get isPasswordValid => RegExp(
+    r'^(?=.*[a-z])(?=.*\d)(?=.*[!@#\$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$',
+  ).hasMatch(this);
 }
