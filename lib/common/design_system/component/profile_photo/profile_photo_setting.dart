24abfb326b12ee86c43a83db65b47fdcb04e4462
SeokYoon Kim <isdebrave@gmail.com>
2025-04-10 01:45:52 +0900

    :lipstick: profile photo에 해당되는 컴포넌트들 생성

---
+++ lib/common/design_system/component/profile_photo/profile_photo_setting.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:frontend_mobile/common/design_system/component/profile_photo/profile_photo_btc.dart';
+import 'package:frontend_mobile/common/design_system/component/profile_photo/profile_photo_size.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CustomProfilePhotoSetting extends StatefulWidget {
+  const CustomProfilePhotoSetting.girl({super.key}) : isGirl = true;
+
+  const CustomProfilePhotoSetting.boy({super.key}) : isGirl = false;
+
+  final bool isGirl;
+
+  @override
+  State<CustomProfilePhotoSetting> createState() =>
+      _CustomProfilePhotoSettingState();
+}
+
+class _CustomProfilePhotoSettingState extends State<CustomProfilePhotoSetting> {
+  File? _imageFile;
+  final ImagePicker imagePicker = ImagePicker();
+
+  Widget get _image {
+    if (widget.isGirl) {
+      return CustomProfilePhotoSize.girl(onPressed: null);
+    }
+
+    return CustomProfilePhotoSize.boy(onPressed: null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _imageFile == null
+            ? _image
+            : Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(shape: BoxShape.circle),
+              child: Image.file(
+                _imageFile!,
+                width: 90,
+                height: 90,
+                fit: BoxFit.cover,
+              ),
+            ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: CustomProfilePhotoBtc(
+            type: CustomProfilePhotoBtcType.camera,
+            onPressed: () async {
+              final XFile? xFile = await imagePicker.pickImage(
+                source: ImageSource.gallery,
+              );
+
+              if (xFile != null) {
+                setState(() {
+                  _imageFile = File(xFile.path);
+                });
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
