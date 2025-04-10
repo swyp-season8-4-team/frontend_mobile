3f6b4eae55641910e26506999b303e89e556adb1
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-10 11:19:43 +0900

    :sparkles: Preference API 연동 추가

---
+++ lib/data/data_source/remote/preference_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend_mobile/core/resource/network/app_dio.dart';
+import 'package:frontend_mobile/data/entity/preference/preference_entity.dart';
+import 'package:retrofit/http.dart';
+
+part 'generated/preference_remote_data_source.g.dart';
+
+final Provider<PreferenceRemoteDataSource> preferenceApiProvider =
+    Provider<PreferenceRemoteDataSource>((Ref ref) {
+      return PreferenceRemoteDataSource(ref.read(appDioProvider));
+    });
+
+@RestApi()
+abstract class PreferenceRemoteDataSource {
+  factory PreferenceRemoteDataSource(Dio dio) = _PreferenceRemoteDataSource;
+
+  @GET('/api/preferences')
+  Future<List<PreferenceEntity>> getAllPreferences();
+}
