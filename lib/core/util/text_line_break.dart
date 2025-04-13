66468ac8ba191206b3c3709c946e927d5ba4d8f6
SeokYoon Kim <isdebrave@gmail.com>
2025-04-13 16:02:52 +0900

    :sparkles: text_lilne_break 체리픽

---
+++ lib/core/util/text_line_break.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextLineBreak extends StatelessWidget {
+  const TextLineBreak({required this.text, required this.style, super.key});
+
+  final String text;
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children:
+          text
+              .split(' ')
+              .map((String word) => Text('$word ', style: style))
+              .toList(),
+    );
+  }
+}
