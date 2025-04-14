7130f961aa9c812f940e6fa9fc067aa53f7b3146
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-15 02:09:29 +0900

    :lipstick: 사장님 픽 이미지 갤러리 UI 구현

--- lib/presentation/map/store_detail/store_detail_view.dart
+++ lib/presentation/map/store_detail/store_detail_view.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend_mobile/common/design_system/component/top_bar/sub_top_bar.dart';
+import 'package:frontend_mobile/common/design_system/foundation/color/scale_color_config.dart';
+import 'package:frontend_mobile/common/gen_asset/assets.gen.dart';
+
+part 'local_widget/owner_pick_image.dart';
 
 class StoreDetailView extends StatefulWidget {
   const StoreDetailView({required this.storeUuid, super.key});
@@ -13,7 +19,10 @@ class _StoreDetailViewState extends State<StoreDetailView> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: CustomSubTopBar(title: '', actions: <Widget>[]),
+      appBar: CustomSubTopBar(
+        title: '',
+        actions: <Widget>[_OwnerPickImage(imageUrls: <String>[])],
+      ),
     );
   }
 }
