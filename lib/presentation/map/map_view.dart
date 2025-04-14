a52c86079aa5878e15c417aa7c587f1ff3a97311
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-14 16:42:33 +0900

    :recycle: 저장 리스트 전체 조회 UI 구현 방식 변경

--- lib/presentation/map/map_view.dart
+++ lib/presentation/map/map_view.dart
@@ -8,6 +8,7 @@ import 'package:frontend_mobile/common/design_system/component/chip/floating_chi
 import 'package:frontend_mobile/common/design_system/component/etc/map/map_icon_button.dart';
 import 'package:frontend_mobile/common/design_system/component/etc/map/saved_store_list.dart';
 import 'package:frontend_mobile/common/design_system/component/etc/option_menu_dropdown.dart';
+import 'package:frontend_mobile/common/design_system/component/navigation_bar/navigation_bar.dart';
 import 'package:frontend_mobile/common/design_system/component/top_bar/main_top_bar.dart';
 import 'package:frontend_mobile/common/design_system/component/top_bar/resource/top_bar_icon.dart';
 import 'package:frontend_mobile/common/design_system/component/top_bar/sub_top_bar.dart';
@@ -17,8 +18,9 @@ import 'package:frontend_mobile/core/manager/geolocation/geo_location_service_im
 import 'package:frontend_mobile/core/resource/status.dart';
 import 'package:frontend_mobile/domain/model/preference/preference_model.dart';
 import 'package:frontend_mobile/presentation/map/map_view_model.dart';
-import 'package:frontend_mobile/presentation/widget/scaffold_with_navigation_bar.dart';
+import 'package:frontend_mobile/presentation/router/routes.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:go_router/go_router.dart';
 
 class MapView extends ConsumerStatefulWidget {
   const MapView({super.key});
@@ -33,6 +35,9 @@ class _MapViewState extends ConsumerState<MapView> {
   late final NaverMapController _mapController;
   late final DraggableScrollableController _draggableScrollableController;
 
+  // draggable scrollable sheet의 snap size
+  final double _snapSize = 0.5;
+
   bool _isStoreListAppbarVisible = false;
 
   @override
@@ -43,12 +48,7 @@ class _MapViewState extends ConsumerState<MapView> {
   }
 
   void _draggableScrollableListener() {
-    final Size size = MediaQuery.of(context).size;
-    final EdgeInsets mediaQueryPadding = MediaQuery.of(context).padding;
-    final double maxChildSize =
-        1 - (kToolbarHeight + mediaQueryPadding.top) / size.height - 0.05;
-
-    if (_draggableScrollableController.size >= maxChildSize) {
+    if (_draggableScrollableController.size >= 0.99) {
       if (!_isStoreListAppbarVisible) {
         setState(() {
           _isStoreListAppbarVisible = true;
@@ -74,18 +74,28 @@ class _MapViewState extends ConsumerState<MapView> {
     }
 
     final TopBarIcon topBarIcon = TopBarIcon();
+    final TextTheme textTheme = Theme.of(context).textTheme;
 
-    return ScaffoldWithNavigationBar(
+    return Scaffold(
       appBar:
           _isStoreListAppbarVisible
               ? CustomSubTopBar(
                 title: '찜한 가게',
                 primary: false,
+                leading: topBarIcon.leftLine(
+                  onTap: () {
+                    _draggableScrollableController.animateTo(
+                      0,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
+                  },
+                ),
                 actions: <Widget>[
                   topBarIcon.close(
                     onTap: () {
                       _draggableScrollableController.animateTo(
-                        0.4,
+                        _snapSize,
                         duration: const Duration(milliseconds: 300),
                         curve: Curves.easeIn,
                       );
@@ -96,23 +106,29 @@ class _MapViewState extends ConsumerState<MapView> {
               : const CustomMainTopBar(),
       body: Stack(
         children: <Widget>[
-          NaverMap(
-            options: NaverMapViewOptions(
-              initialCameraPosition: NCameraPosition(
-                // TODO: 첫 로딩 시 카메라의 위치를 임의로 강남구로 지정
-                target: const NLatLng(37.514575, 127.0495556),
-                zoom: _zoom,
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 80,
+            child: NaverMap(
+              options: NaverMapViewOptions(
+                initialCameraPosition: NCameraPosition(
+                  // TODO: 첫 로딩 시 카메라의 위치를 임의로 강남구로 지정
+                  target: const NLatLng(37.514575, 127.0495556),
+                  zoom: _zoom,
+                ),
               ),
+              onMapReady: (NaverMapController controller) async {
+                _mapController = controller;
+
+                await _goToCurrentLocation();
+
+                await _setCustomLocationOverlay();
+
+                setState(() {});
+              },
             ),
-            onMapReady: (NaverMapController controller) async {
-              _mapController = controller;
-
-              await _goToCurrentLocation();
-
-              await _setCustomLocationOverlay();
-
-              setState(() {});
-            },
           ),
           Positioned(
             top: 16,
@@ -156,7 +172,11 @@ class _MapViewState extends ConsumerState<MapView> {
                 CustomMapIconButton.saveStore(
                   isSelected: true,
                   onPressed: () async {
-                    await _showStoreList();
+                    await _draggableScrollableController.animateTo(
+                      _snapSize,
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
                   },
                 ),
                 const SizedBox(height: 10),
@@ -169,8 +189,143 @@ class _MapViewState extends ConsumerState<MapView> {
               ],
             ),
           ),
-          if (_isStoreListAppbarVisible)
-            Positioned.fill(child: Container(color: ScaleColorConfig.white)),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: CustomNavigationBar(
+              list: <NavigationBarType>[
+                NavigationBarType(
+                  svg: Assets.icon.map.mapLine,
+                  label: '지도',
+                  onTap: () {
+                    context.goNamed(AppRoutes.map.name);
+                  },
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            top: 0,
+            child: DraggableScrollableSheet(
+              controller: _draggableScrollableController,
+              minChildSize: 0,
+              initialChildSize: 0,
+              snap: true,
+              expand: false,
+              snapSizes: <double>[_snapSize],
+              builder: (
+                BuildContext context,
+                ScrollController scrollController,
+              ) {
+                return SafeArea(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft:
+                            _isStoreListAppbarVisible
+                                ? Radius.zero
+                                : const Radius.circular(28),
+                        topRight:
+                            _isStoreListAppbarVisible
+                                ? Radius.zero
+                                : const Radius.circular(28),
+                      ),
+                      color: ScaleColorConfig.white,
+                    ),
+                    child: CustomScrollView(
+                      controller: scrollController,
+                      slivers: <Widget>[
+                        _isStoreListAppbarVisible
+                            ? const SliverToBoxAdapter(child: SizedBox.shrink())
+                            : SliverToBoxAdapter(
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16,
+                                ),
+                                alignment: Alignment.center,
+                                child: Container(
+                                  width: 32,
+                                  height: 4,
+                                  decoration: BoxDecoration(
+                                    color: ScaleColorConfig.neutral30,
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                ),
+                              ),
+                            ),
+                        SliverToBoxAdapter(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 10,
+                              bottom: 6,
+                              right: 16,
+                              left: 16,
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  '전체 리스트',
+                                  style: textTheme.titleMedium?.copyWith(
+                                    color: ScaleColorConfig.primary20,
+                                  ),
+                                ),
+                                const SizedBox(width: 6),
+                                Text(
+                                  '9',
+                                  style: textTheme.titleMedium?.copyWith(
+                                    color: ScaleColorConfig.success50,
+                                  ),
+                                ),
+                                const Spacer(),
+                                CustomOutlineButton.xSmall(
+                                  label: '새 리스트 추가',
+                                  onPressed: () {},
+                                  svg: Assets.icon.system.addCircleLine,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SliverList(
+                          delegate: SliverChildListDelegate(
+                            List<Widget>.generate(20, (int index) {
+                              return Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 16,
+                                    ),
+                                    child:
+                                        CustomSavedStoreListItem.withOptionMenus(
+                                          leftIconColor: Colors.red,
+                                          name: '디저트 가게 모음 $index',
+                                          storeLength: 4,
+                                          optionMenus:
+                                              const <CustomOptionMenu>[],
+                                          optionMenusVisible: false,
+                                          onOptionMenusTap: () {},
+                                          onTap: () {},
+                                        ),
+                                  ),
+                                  const Divider(
+                                    color: ScaleColorConfig.neutral50,
+                                  ),
+                                ],
+                              );
+                            }),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -217,127 +372,6 @@ class _MapViewState extends ConsumerState<MapView> {
         ..setIsVisible(false)
         ..setIsVisible(true);
     }
-  }
-
-  Future<void> _showStoreList() async {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    final Size size = MediaQuery.of(context).size;
-    final EdgeInsets mediaQueryPadding = MediaQuery.of(context).padding;
-
-    await showModalBottomSheet(
-      context: context,
-      backgroundColor: ScaleColorConfig.white,
-      barrierColor: Colors.transparent,
-      constraints: const BoxConstraints(minWidth: double.infinity),
-      isScrollControlled: true,
-      builder: (BuildContext context) {
-        return DraggableScrollableSheet(
-          controller: _draggableScrollableController,
-          minChildSize: 0.4,
-          initialChildSize: 0.4,
-          maxChildSize:
-              1 - (kToolbarHeight + mediaQueryPadding.top) / size.height,
-          snap: true,
-          expand: false,
-          snapSizes: const <double>[0.4],
-          builder: (BuildContext context, ScrollController scrollController) {
-            return SafeArea(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft:
-                        _isStoreListAppbarVisible
-                            ? Radius.zero
-                            : const Radius.circular(28),
-                    topRight:
-                        _isStoreListAppbarVisible
-                            ? Radius.zero
-                            : const Radius.circular(28),
-                  ),
-                ),
-                child: CustomScrollView(
-                  controller: scrollController,
-                  slivers: <Widget>[
-                    SliverToBoxAdapter(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 32,
-                          height: 4,
-                          decoration: BoxDecoration(
-                            color: ScaleColorConfig.neutral30,
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SliverToBoxAdapter(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                          bottom: 6,
-                          right: 16,
-                          left: 16,
-                        ),
-                        child: Row(
-                          children: <Widget>[
-                            Text(
-                              '전체 리스트',
-                              style: textTheme.titleMedium?.copyWith(
-                                color: ScaleColorConfig.primary20,
-                              ),
-                            ),
-                            const SizedBox(width: 6),
-                            Text(
-                              '9',
-                              style: textTheme.titleMedium?.copyWith(
-                                color: ScaleColorConfig.success50,
-                              ),
-                            ),
-                            const Spacer(),
-                            CustomOutlineButton.xSmall(
-                              label: '새 리스트 추가',
-                              onPressed: () {},
-                              svg: Assets.icon.system.addCircleLine,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SliverList(
-                      delegate: SliverChildListDelegate(
-                        List<Widget>.generate(20, (int index) {
-                          return Column(
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 16,
-                                ),
-                                child: CustomSavedStoreListItem.withOptionMenus(
-                                  leftIconColor: Colors.red,
-                                  name: '디저트 가게 모음 $index',
-                                  storeLength: 4,
-                                  optionMenus: const <CustomOptionMenu>[],
-                                  optionMenusVisible: false,
-                                  onOptionMenusTap: () {},
-                                  onTap: () {},
-                                ),
-                              ),
-                              const Divider(color: ScaleColorConfig.neutral50),
-                            ],
-                          );
-                        }),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
   }
 
   @override
