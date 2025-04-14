04355369044811ad82eb2e1c1ec97a68ad1bf207
SeokYoon Kim <isdebrave@gmail.com>
2025-04-15 02:25:49 +0900

    :sparkles: 날짜 extension 추가

--- lib/core/resource/extension.dart
+++ lib/core/resource/extension.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 extension StringValidationExt on String {
   bool get isEmail =>
       RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(this);
@@ -5,4 +7,9 @@ extension StringValidationExt on String {
   bool get isPasswordValid => RegExp(
     r'^(?=.*[a-z])(?=.*\d)(?=.*[!@#\$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$',
   ).hasMatch(this);
+
+  String toDate() {
+    final DateTime parsedDate = DateTime.parse(this);
+    return DateFormat('yyyy.MM.dd').format(parsedDate);
+  }
 }
