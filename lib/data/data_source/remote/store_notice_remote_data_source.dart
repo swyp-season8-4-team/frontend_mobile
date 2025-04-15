d43e71268d45bbc85f71f40cdde0a28025af9ce1
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-15 23:25:36 +0900

    :sparkles: 모든 공지 조회 API 연동 추가

---
+++ lib/data/data_source/remote/store_notice_remote_data_source.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend_mobile/core/resource/network/app_dio.dart';
+import 'package:frontend_mobile/data/entity/store_notice/store_notice_entity.dart';
+import 'package:retrofit/http.dart';
+
+part 'generated/store_notice_remote_data_source.g.dart';
+
+final Provider<StoreNoticeRemoteDataSource> storeNoticeApiProvider =
+    Provider<StoreNoticeRemoteDataSource>((Ref ref) {
+      return StoreNoticeRemoteDataSource(ref.read(appDioProvider));
+    });
+
+@RestApi()
+abstract class StoreNoticeRemoteDataSource {
+  factory StoreNoticeRemoteDataSource(Dio dio) = _StoreNoticeRemoteDataSource;
+
+  @GET('/api/stores/{storeUuid}/notices')
+  Future<List<StoreNoticeEntity>> getStoreNotices({
+    @Path() required String storeUuid,
+  });
+}
