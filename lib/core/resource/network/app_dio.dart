2778b80d86c7b5a46829ae78a57610a0f140421d
SeokYoon Kim <isdebrave@gmail.com>
2025-04-20 03:59:25 +0900

    :recycle: 회원가입할 때는 header에 accessToken 안 넣도록 수정

--- lib/core/resource/network/app_dio.dart
+++ lib/core/resource/network/app_dio.dart
@@ -1,11 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:frontend_mobile/common/env/env.dart';
 import 'package:frontend_mobile/core/resource/constant.dart';
 import 'package:frontend_mobile/core/resource/secure_storage/secure_storage.dart';
 import 'package:frontend_mobile/core/resource/token_info.dart';
+import 'package:frontend_mobile/presentation/router/router.dart';
+import 'package:frontend_mobile/presentation/router/routes.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 final Provider<Dio> appDioProvider = Provider<Dio>((Ref ref) {
@@ -15,17 +19,40 @@ final Provider<Dio> appDioProvider = Provider<Dio>((Ref ref) {
   ) async {
     /// TODO: 토큰이 만료되면, refreshToken로 새로운 토큰 받아오고,
     /// refreshToken 만료되면 로그인 창으로 이동시켜야 함
-    final FlutterSecureStorage storage = ref.read(secureStorageProvider);
 
-    final String? json = await storage.read(key: Constant.tokenInfo);
+    bool tokenRequired = true;
 
-    if (json != null) {
-      final TokenInfo tokenInfo = TokenInfo.deserialize(json: json);
-      final String accessToken = tokenInfo.accessToken;
+    if (AppRouter.rootNavigatorKey.currentContext != null) {
+      final BuildContext context = AppRouter.rootNavigatorKey.currentContext!;
+      final GoRouterState route = GoRouter.of(context).routerDelegate.state;
 
-      options.headers['Authorization'] = 'Bearer $accessToken';
-      options.headers['Platform-Type'] = 'app';
+      if (route.name != null) {
+        /// 회원가입
+        if (route.name!.contains(AppRoutes.signUp.name)) {
+          tokenRequired = false;
+        }
+
+        /// 로그인
+        // if (route.name!.contains(AppRoutes.localLogin.name)) {
+        //   tokenRequired = false;
+        // }
+      }
     }
+
+    if (tokenRequired) {
+      final FlutterSecureStorage storage = ref.read(secureStorageProvider);
+
+      final String? json = await storage.read(key: Constant.tokenInfo);
+
+      if (json != null) {
+        final TokenInfo tokenInfo = TokenInfo.deserialize(json: json);
+        final String accessToken = tokenInfo.accessToken;
+
+        options.headers['Authorization'] = 'Bearer $accessToken';
+      }
+    }
+
+    options.headers['Platform-Type'] = 'app';
 
     handler.next(options);
   }
