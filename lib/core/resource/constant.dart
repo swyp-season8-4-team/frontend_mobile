ce584ed7c76df5557f0b3c72c36e752af93dbf28
SeokYoon Kim <isdebrave@gmail.com>
2025-04-19 12:14:20 +0900

    :truck: 파일 위치 수정

--- lib/core/resource/constant.dart
+++ lib/core/resource/constant.dart
@@ -40,3 +40,13 @@ enum RegularClosureType {
     );
   }
 }
+
+/// 닉네임 인증 호출 목적
+enum Nickname {
+  signUp(value: 'SIGNUP'),
+  profileUpdate(value: 'PROFILE_UPDATE');
+
+  const Nickname({required this.value});
+
+  final String value;
+}
--- lib/core/resource/nickname.dart
+++
@@ -1,9 +0,0 @@
-/// 닉네임 인증 호출 목적
-enum Nickname {
-  signUp(value: 'SIGNUP'),
-  profileUpdate(value: 'PROFILE_UPDATE');
-
-  const Nickname({required this.value});
-
-  final String value;
-}
