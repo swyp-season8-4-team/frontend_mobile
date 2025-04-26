c7acbf9f24e6d64cb48af09818df163f87c8139f
SeokYoon Kim <isdebrave@gmail.com>
2025-04-26 20:09:34 +0900

    :recycle: 상세 정보 데이터 연동

--- lib/presentation/dessert/post/dessert_post_view.dart
+++ lib/presentation/dessert/post/dessert_post_view.dart
@@ -60,37 +60,26 @@ class _DessertPostState extends ConsumerState<DessertPost> {
                       child: DessertPostHeaderInfo(),
                     ),
 
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                        horizontal: 16,
-                      ),
-                      decoration: const BoxDecoration(
-                        border: Border(
-                          top: BorderSide(color: ScaleColorConfig.neutral70),
-                          bottom: BorderSide(color: ScaleColorConfig.neutral70),
+                    if (state.data.place?.placeName != null)
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                          horizontal: 16,
                         ),
+                        decoration: const BoxDecoration(
+                          border: Border(
+                            top: BorderSide(color: ScaleColorConfig.neutral70),
+                            bottom: BorderSide(
+                              color: ScaleColorConfig.neutral70,
+                            ),
+                          ),
+                        ),
+                        child: const DessertPostHeaderLocation(),
                       ),
-                      child: const DessertPostHeaderLocation(),
-                    ),
 
-                    const Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Text(
-                        '''안녕하세요, 디저트 사랑하는 여러분! 🍰💕
-          
-          망원동에서 맛있는 베이커리 카페를 탐험할 사람을 모집합니다!
-            
-          🥐 일정: 이번 주말
-            
-          📍 장소: 망원동의 숨겨진 베이커리 카페
-            
-          🎯 목표: 맛있는 빵과 커피를 즐기고, 인생의 소소한 행복을 만끽하기!
-            
-          참고로, 단순히 먹는 것만이 목표는 아닙니다. 서로의 디저트 취향을 공유하고, 재미있는 이야기들도 나누면서 즐거운 시간을 보내고 싶어요! 😋😄
-            
-          참여하고 싶은 분은 댓글로 "저도 가요!"라고 외쳐주세요! 귀여운 디저트 친구가 되어줄 사람, 기다리고 있을게요! 🤗''',
-                      ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text(state.data.content),
                     ),
 
                     Container(
