fe4d15bcce449f7220e1a4c552ef3fabf8ddaf27
generalChang <rhdrnsckdgh0921@naver.com>
2025-04-29 15:49:05 +0900

    :sparkles: 마이페이지 > Default > 디저트 메이트 페이지 (MyDessertMateView) 생성

---
+++ lib/presentation/my_page/dessert_mate/my_dessert_mate_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyDessertMateView extends StatefulWidget {
+  const MyDessertMateView({super.key});
+
+  @override
+  State<MyDessertMateView> createState() => _MyDessertMateViewState();
+}
+
+class _MyDessertMateViewState extends State<MyDessertMateView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
