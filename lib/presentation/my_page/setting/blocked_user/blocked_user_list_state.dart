7185acdced358c36109274f722f5dc75316a82c4
generalChang <rhdrnsckdgh0921@naver.com>
2025-05-17 23:34:44 +0900

    :lipstick: 차단 유저 관리 관련 상태 관리 로직 추가

---
+++ lib/presentation/my_page/setting/blocked_user/blocked_user_list_state.dart
@@ -0,0 +1,18 @@
+part of 'blocked_user_list_view_model.dart';
+
+@freezed
+class BlockedUserListState with _$BlockedUserListState {
+  factory BlockedUserListState({
+    @Default(
+      BlockedUserListModel(blockedUsers: <BlockedUserModel>[], totalCount: -1),
+    )
+    BlockedUserListModel blockedList,
+    @Default(Status.loading) Status getBlockedUsersStatus,
+    @Default(ExceptionModel(status: -1, code: '', message: '', timestamp: ''))
+    ExceptionModel getBlockedUsersException,
+
+    @Default(Status.initial) Status unblockUserStatus,
+    @Default(ExceptionModel(status: -1, code: '', message: '', timestamp: ''))
+    ExceptionModel unblockUserException,
+  }) = _BlockedUserListState;
+}
